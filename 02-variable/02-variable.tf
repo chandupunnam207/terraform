2b29cb81178296101e310194a27af0338581dce3
Punnam <chandu.punnam207@gmail.com>
2021-07-26 22:31:20 +0530

    Variable project output

--- 02-variable/02-variable.tf
+++ 02-variable/02-variable.tf
@@ -9,11 +9,11 @@ variable "test2" {
 }
 
 output test1 {
-  value = "var.test1"
+  value = var.test1
 }
 
 output test2 {
-  value = "var.test2"
+  value = var.test2
 }
 
 
@@ -22,11 +22,11 @@ variable Trainings {
 }
 
 output First_Training {
-  value = "var.Trainings[0]"
+  value = var.Trainings[0]
 }
 
 output Second_Training {
-  value = "var.Trainings[1]"
+  value = var.Trainings[1]
 }
 
 
